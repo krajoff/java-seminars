d9d3bb317fa77f1f7ec7005bfa3e74b6d0818039
krajoff <iioz@ya.ru>
2023-01-21 22:00:38 +0300

    Solved just 4 tasks

--- lesson_1/src/Main.java
+++ lesson_1/src/Main.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ex4("2? + ?5 = 69");
+        ex0();
+        ex1();
+        ex2();
+        ex3();
     }
 
     // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
     static void ex0() {
-        System.out.println("Введите целое число: ");
+        System.out.print("Input integer number: ");
         Scanner scanner = new Scanner(System.in);
         int number;
         number = scanner.nextInt();
         int result = number * (number + 1) / 2;
-        System.out.printf("Треугольное число для %d = %d", number, result);
+        System.out.printf("For %d = %d", number, result);
+        System.out.println();
     }
 
     // Вычислить n! (произведение чисел от 1 до n)
     static void ex1() {
-        System.out.println("Введите целое число: ");
+        System.out.print("Input integer number: ");
         Scanner scanner = new Scanner(System.in);
         int number;
         int result = 1;
@@ -27,11 +31,12 @@ public class Main {
             result *= i;
         }
         System.out.printf("%d! = %d", number, result);
+        System.out.println();
     }
 
     // Вывести все простые числа от 1 до 1000
     static void ex2() {
-        System.out.println("Простые числа от 1 до 1000: 1 ");
+        System.out.println("Prime number from 1 till 1000: 1 ");
         boolean flag;
         for (int i = 2; i < 1000; i++) {
             flag = true;
@@ -43,6 +48,7 @@ public class Main {
             }
             if (flag) System.out.printf("%d ", i);
         }
+        System.out.println();
     }
 
     /*
@@ -51,12 +57,12 @@ public class Main {
     выполнить с этими числами"... "ответ: ...")
     */
     static void ex3() {
-        System.out.print("Введите первое число: ");
+        System.out.print("Input first number: ");
         Scanner scan = new Scanner(System.in);
         int num1 = scan.nextInt();
-        System.out.print("Введите второе число: ");
+        System.out.print("Input second number: ");
         int num2 = scan.nextInt();
-        System.out.print("Укажите операцию: ");
+        System.out.print("Input operand: ");
         char opt = scan.next().charAt(0);
         int result = -1;
         switch (opt) {
@@ -74,27 +80,6 @@ public class Main {
                 break;
         }
         System.out.printf("%d %s %d = %d", num1, opt, num2, result);
-    }
-
-    /*
-    Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут
-    быть заменены знаком вопроса, например 2? + ?5 = 69. Требуется восстановить
-    выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
-    */
-    static String ex4(String str) {
-        String[] eq = str.split(" ");
-        String result = null;
-        System.out.println(Arrays.toString(eq));
-
-        for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 10; j++){
-
-                char[] chars = str.toCharArray();
-
-            }
-        }
-
-        return result;
     }
 }
 
