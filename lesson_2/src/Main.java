bd93769bc4a772b921ab9d74d06e8327ac06079f
krajoff <t0k@bk.ru>
2023-01-26 10:43:17 +0300

    Solved

--- lesson_2/src/Main.java
+++ lesson_2/src/Main.java
@@ -10,19 +10,41 @@ import java.util.logging.SimpleFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder = new StringBuilder("TEST");
-        String filePath = "test.txt";
-        ex2(stringBuilder, filePath);
+        String strJSON = "{{\"фамилия\":\"Иванов\", \"оценка\":\"5\", \"предмет\":\"Математика\"},\n" +
+                "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},\n" +
+                "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}}";
+        StringBuilder stringBuilder = ex0(strJSON);
+        ex1(stringBuilder, "output.txt");
     }
 
+    /*
+     * Дана json строка { { "фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":
+     * "Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}} Задача написать метод(ы), который распарсить строку
+     * и выдаст ответ вида: Студент Иванов получил 5 по предмету Математика. Студент Петрова получил 4 по предмету Информатика.
+     * Студент Краснов получил 5 по предмету Физика. Используйте StringBuilder для подготовки ответа
+     */
+    static StringBuilder ex0(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] strStudent = str.split("},");
+        for (int i = 0; i < strStudent.length; i++) {
+            String[] strInfoStudent = strStudent[i].split(",");
+            for (int j = 0; j < strInfoStudent.length; j++) {
+                String[] strInfoByPart = strInfoStudent[j].split(":");
+                strInfoStudent[j] = strInfoByPart[1].replace("\"", "");
+                strInfoStudent[j] = strInfoStudent[j].replace("}", "");
+            }
+            stringBuilder = stringBuilder.append("Студент " + strInfoStudent[0] + " получил " + strInfoStudent[1] + " по предмету " + strInfoStudent[2] + ".\n");
+        }
+        System.out.println(stringBuilder);
+        return stringBuilder;
+    }
 
-    static void ex2(StringBuilder stringBuilder, String filePath){
-
+    // Создать метод, который запишет результат работы в файл Обработайте исключения и запишите ошибки в лог файл
+    static void ex1(StringBuilder stringBuilder, String filePath) {
         Logger logger = Logger.getAnonymousLogger();
-        logger.log(Level.INFO, "So bad");
         SimpleFormatter formatter = new SimpleFormatter();
         FileHandler fileHandler = null;
+
         try {
             fileHandler = new FileHandler("log.txt");
             fileHandler.setFormatter(formatter);
@@ -30,9 +52,10 @@ public class Main {
             e.printStackTrace();
         }
         logger.addHandler(fileHandler);
+
         try (FileWriter fileWriter = new FileWriter(filePath, false)) {
             fileWriter.write(stringBuilder.toString());
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.log(Level.WARNING, e.getMessage());
             e.printStackTrace();
         }
