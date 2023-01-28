80a778b8a27c405cb334537810330d030a2ab8e8
krajoff <iioz@ya.ru>
2023-01-28 21:19:55 +0300

    Solved tasks

--- lesson_3/src/Main.java
+++ lesson_3/src/Main.java
@@ -1,29 +1,39 @@
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         ex0(10);
-
-
-        ex1();
+        ex1(3);
     }
 
     // Пусть дан произвольный список целых чисел, удалить из него четные числа
     static void ex0(int number) {
+        Random random = new Random();
         List<Integer> list = new ArrayList();
         for (int i = 0; i < number; i++) {
-            list.add(i, (int) (Math.random() * 10));
+            list.add(random.nextInt(10));
         }
-        System.out.println("Исходный массив: " + list);
+        System.out.println("Initial array: " + list);
         list.removeIf(x -> x % 2 == 0);
-        System.out.println("Новый массив: " + list);
+        System.out.println("New array: " + list);
     }
 
     // Задан целочисленный список ArrayList. Найти минимальное, максимальное и
     // среднее арифметическое из этого списка. Collections.max()
-    static void ex1() {
+    static void ex1(int number) {
+        Random random = new Random();
+        List<Integer> list = new ArrayList();
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            list.add(random.nextInt(10));
+            sum += list.get(i);
+        }
+        double average = (double)sum/number;
+        System.out.println("Array: " + list);
+        System.out.println("Maximum of array: " + Collections.max(list));
+        System.out.println("Minimum of array: " + Collections.min(list));
+        System.out.println("Average of array: " + average);
 
     }
 }
