9f455eee24da1a3ab9908c087a346884ee92032c
krajoff <t0k@bk.ru>
2023-01-31 16:24:03 +0300

    First attempt

--- lesson_3/src/Main.java
+++ lesson_3/src/Main.java
@@ -5,9 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ex0(10);
-
-
-        ex1();
+        ex1(10);
     }
 
     // Пусть дан произвольный список целых чисел, удалить из него четные числа
@@ -23,7 +21,10 @@ public class Main {
 
     // Задан целочисленный список ArrayList. Найти минимальное, максимальное и
     // среднее арифметическое из этого списка. Collections.max()
-    static void ex1() {
-
+    static void ex1(int number) {
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < number; i++) {
+            list.add(i, (int) (Math.random() * 10));
+        }
     }
 }
