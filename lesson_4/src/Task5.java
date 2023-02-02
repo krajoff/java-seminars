eb791bf4a0fa667754216725776ac39ce05d07cf
krajoff <iioz@ya.ru>
2023-02-02 22:41:47 +0300

    Task5

---
+++ lesson_4/src/Task5.java
@@ -0,0 +1,14 @@
+public class Task5 {
+    public static void main(String[] args) {
+//        String n = "4";
+//        String d = "15 2 9 3";
+        String n = "5";
+        String d = "4 16 4 9 5";
+        ex(n, d);
+    }
+        static void ex(String num, String str) {
+            int n = Integer.parseInt(num);
+            String[] frequencies = str.split(" ");
+
+    }
+}
