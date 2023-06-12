1436766ded917b9f5f5aaad40057b6baaeb126fa
krajoff <iioz@ya.ru>
2023-06-12 16:49:35 +0300

    Exception

--- Exceptions/src/Account.java
+++ Exceptions/src/Account.java
@@ -5,13 +5,11 @@ import java.text.ParseException;
 
 public class Account {
     public static void main(String[] args) throws ParseException {
-        String initial = "Николай Яшин Валерьевич 29.02.1999 892255542164 m";
+        String initial = "Николай Яшин Валерьевич 28.02.1999 892255542164 m";
         person person = validator.push(initial);
+        System.out.println(person);
        // file.write(data);
 
-
-
-        System.out.println("2");
     }
 
 
--- Exceptions/src/account/checkSex.java
+++
@@ -1,9 +0,0 @@
-package account;
-
-public class checkSex {
-    public static void isValid(String sex) {
-        if (!sex.equals("m") && !sex.equals("f")) {
-            throw new RuntimeException("You entered incorrect sex");
-        }
-    }
-}
