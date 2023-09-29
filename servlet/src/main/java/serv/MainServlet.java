37a7e7682e2146cbea21c39a1dc477edc7bdeae4
krajoff <t0k@bk.ru>
2023-09-29 16:29:20 +0300

    Add lesson 4

--- servlet/src/main/java/serv/MainServlet.java
+++ servlet/src/main/java/serv/MainServlet.java
@@ -1,2 +1,26 @@
-package serv;public class MainServlet {
+package serv;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 }
