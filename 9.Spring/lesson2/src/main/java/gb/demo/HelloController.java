86f87dd9851ee597a27c60f18f2f7468a8fb2ab0
krajoff <iioz@ya.ru>
2024-01-13 18:00:54 +0300

    Lesson2

--- 9.Spring/lesson2/src/main/java/gb/demo/HelloController.java
+++ 9.Spring/lesson2/src/main/java/gb/demo/HelloController.java
@@ -4,11 +4,20 @@ package gb.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @GetMapping("/")
     public String ola() {
-        return "What's up!";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return "Вы зашли в " + LocalTime.now().format(formatter);
     }
 }
